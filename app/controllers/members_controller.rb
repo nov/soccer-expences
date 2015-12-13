ffbdcfd45196522b6c20486f1ab126a9fb645209
nov <nov@matake.jp>
2015-12-14 01:29:23 +0900

    ACL for members

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,12 @@
 class MembersController < ApplicationController
+  before_filter :require_approved_access
+  before_filter :require_admin_access, only: :create
+
   def index
     @members = Member.all
+  end
+
+  def create
+
   end
 end
