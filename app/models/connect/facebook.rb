00dd43f103a4c4f5f7a132e5690c7e38c6b2c5c1
nov <nov@matake.jp>
2015-12-13 18:58:02 +0900

    current user display name

--- app/models/connect/facebook.rb
+++ app/models/connect/facebook.rb
@@ -3,13 +3,6 @@ require 'url_safe_base64'
 class Connect::Facebook < ActiveRecord::Base
   belongs_to :account
 
-  def picture(type = :square)
-    [
-      File.join(FbGraph2.root_url, identifier, 'picture'),
-      {type: type}.to_query
-    ].join('?')
-  end
-
   def profile
     @profile ||= FbGraph2::User.me(access_token).fetch(
       fields: [:name, :email]
