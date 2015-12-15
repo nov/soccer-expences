186417da9d16444025d2b0767cf3aa3c9406b425
nov <nov@matake.jp>
2015-12-15 16:07:46 +0900

    seed is useful for staging too

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-if Rails.env.development?
+unless Rails.env.production?
   Event.create [{
     title: '横浜市大会 初戦',
     location: '日産スタジアム',
@@ -28,4 +28,10 @@ if Rails.env.development?
     location: '日産スタジアム',
     date: '2015-12-26'
   }]
+
+  50.times do |i|
+  Member.create(
+    display_name: "元石川 #{i + 1}号機",
+    description: "モトイシの#{i + 1}番"
+  )
 end
