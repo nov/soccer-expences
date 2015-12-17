9ed5113ab660851caa404ffdf6446aae9323a284
nov <nov@matake.jp>
2015-12-17 12:04:01 +0900

    will consider Event freezing later.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217022408) do
+ActiveRecord::Schema.define(version: 20151214055855) do
 
   create_table "accounts", force: :cascade do |t|
     t.boolean  "admin",             default: false
@@ -43,15 +43,14 @@ ActiveRecord::Schema.define(version: 20151217022408) do
   add_index "event_members", ["event_id", "member_id"], name: "index_event_members_on_event_id_and_member_id", unique: true
 
   create_table "events", force: :cascade do |t|
-    t.string   "title",                                    null: false
-    t.string   "location",                                 null: false
+    t.string   "title",                                null: false
+    t.string   "location",                             null: false
     t.integer  "cost_from_members_budget", default: 0
     t.integer  "cost_from_team_budget",    default: 0
     t.integer  "event_members_count",      default: 0
-    t.date     "date",                                     null: false
-    t.datetime "created_at",                               null: false
-    t.datetime "updated_at",                               null: false
-    t.boolean  "freezed",                  default: false
+    t.date     "date",                                 null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
   create_table "members", force: :cascade do |t|
