16e55532bedd590450b640057f3b2cb9c7d087be
nov <nov@matake.jp>
2015-12-17 16:27:21 +0900

    goodbye test/*

--- test/controllers/connect/facebooks_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Connect::FacebooksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/event/members_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Event::MembersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
