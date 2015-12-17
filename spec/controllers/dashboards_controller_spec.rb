6abd94492b7893ec54d9b10308a53a784e7f0c0c
nov <nov@matake.jp>
2015-12-17 17:54:08 +0900

    spec for dashboard role-based access control

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.describe DashboardsController, type: :controller do
   context 'when authenticated' do
-    before { controller.authenticate create(:account) }
+    let(:current_account) { create(:account) }
+    before { controller.authenticate current_account }
 
     it do
       get :show
@@ -17,6 +18,33 @@ RSpec.describe DashboardsController, type: :controller do
       it 'should lists all events' do
         get :show
         assigns(:events).should match_array all_events
+      end
+    end
+
+    describe 'role-based access control' do
+      render_views
+
+      context 'when pending' do
+        it do
+          get :show
+          response.should_not render_template(partial: 'events/_list')
+        end
+      end
+
+      context 'when viewer' do
+        before { current_account.approve! }
+        it do
+          get :show
+          response.should render_template(partial: 'events/_list')
+        end
+      end
+
+      context 'when admin' do
+        before { current_account.adminize! }
+        it do
+          get :show
+          response.should render_template(partial: 'events/_list')
+        end
       end
     end
   end
